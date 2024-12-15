2fcd5467584eadc819bd4a1dd88e7edd12d12a5d
TechnoMage21 <guptaom782@gmail.com>
2024-12-15 17:15:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import RecipeFinder from './components/RecipeFinder';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <RecipeFinder/>
+    </div>
+  );
+}
+
+export default App;
